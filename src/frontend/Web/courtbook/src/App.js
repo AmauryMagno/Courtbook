f22db678ddd8adc0c78649a5db1d9b432d98522f
Tomaz Pereira <tomazpontes.dev@gmail.com>
2025-03-21 12:00:43 -0300

    feat: Criação da página de cadastro e configuração do React Router

--- src/frontend/Web/courtbook/src/App.js
+++ src/frontend/Web/courtbook/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Cadastro from './pages/Cadastro';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/" element={
+            <header className="App-header">
+              <p>
+                Bem-vindo ao CourtBook
+              </p>
+              <a
+                className="App-link"
+                href="/cadastro"
+              >
+                Ir para Cadastro
+              </a>
+            </header>
+          } />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
