9745b86dc65f5633ea5d29220d4f99ece9da1a8c
pedro.nogueira <pedro.nogueira@siriusgroup.com.br>
2025-04-07 09:47:24 -0300

    feat:middleware token JWT implementado na API Node

--- src/backend/apiGateway/src/routes.js
+++ src/backend/apiGateway/src/routes.js
@@ -1,16 +1,20 @@
 import { Router } from "express";
 import ReservationsController from "./controllers/ReservationsController.js";
+import { verificarToken } from "./middlewares/authorization.js";
 
-const routes = new Router()
+const routes = new Router();
 
-routes.get('/', (req,res)=>{
-    res.status(200).send({
-        title: "Node Express API",
-        version: "0.0.1"
-    });
-})
+routes.get("/", (req, res) => {
+  res.status(200).send({
+    title: "Node Express API",
+    version: "0.0.1",
+  });
+});
 
-routes.post('/reservations',ReservationsController.create)
+routes.post("/reservations", ReservationsController.create);
+routes.get("/reservations", ReservationsController.findAll);
+routes.get("/reservations/:id", ReservationsController.findOne);
+routes.patch("/reservations/:id", ReservationsController.update);
+routes.delete("/reservations/:id", ReservationsController.delete);
 
-
-export default routes
+export default routes;
